8e42d9e001a6244ab446cf26c9129b87b7dc413c
dclark <dclark@student.42.fr>
2022-02-04 12:13:44 +0100

    Makefile module00 ex00

--- Module 00/ex00/megaphone.cpp
+++ Module 00/ex00/megaphone.cpp
@@ -6,11 +6,21 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 16:01:52 by dclark            #+#    #+#             */
-/*   Updated: 2022/02/02 16:14:48 by dclark           ###   ########.fr       */
+/*   Updated: 2022/02/04 12:13:20 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+
+class	Megaphone {
+
+public:
+	
+	Megaphone(void);
+	~Megaphone(void);
+
+
+};
 
 int main(int ac, char**)
 {
@@ -20,5 +30,4 @@ int main(int ac, char**)
 		std::cin >> buff;
 		std::cout << buff;
 	}
-	//std::cout << std::endl;
 }
