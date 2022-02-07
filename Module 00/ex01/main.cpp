1c286af4680cce1dc5e629df10ebdac46701731b
dclark <dclark@student.42.fr>
2022-02-07 15:09:40 +0100

    copie de cette branch car oublié de commit avant de checkout et commit sur une autre branch

--- Module 00/ex01/main.cpp
+++ Module 00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 12:08:24 by dclark            #+#    #+#             */
-/*   Updated: 2022/02/07 15:06:30 by dclark           ###   ########.fr       */
+/*   Updated: 2022/02/07 15:08:48 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ int main(int ac, char **av)
 
 	while (PB.getExit() == 0) {
 		std::cout << "Please enter a command: ADD, SEARCH or EXIT: ";
-		std::getline (std::cin, PB.command);
+		std::cin >> PB.command;
 		if (check_cmd(PB.command) == -1 || strlen(PB.command) == 0) {
 			std::cout << "Wrong command !" << std::endl;
 		} else if (check_cmd(PB.command) == 1) {
