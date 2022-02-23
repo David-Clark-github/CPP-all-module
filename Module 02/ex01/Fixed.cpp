4fc9ea8ac3744d8ef9981c66d0297f3d2bb0f7f6
dclark <dclark@student.42.fr>
2022-02-23 15:14:20 +0100

    go go save

---
+++ Module 02/ex01/Fixed.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 13:10:01 by dclark            #+#    #+#             */
+/*   Updated: 2022/02/23 15:14:06 by dclark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.h"
+
+Fixed::Fixed(void):_value(0) {
+    std::cout << "Default Constructor called" << std::endl;
+    return;
+}
+
+Fixed::Fixed(const Fixed &f) {
+	std::cout << "Copy constructor called" << std::endl;
+    *this = f;
+    return;
+}
+
+Fixed::~Fixed(void) {
+    std::cout << "Default destructor called" << std::endl;
+}
+
+Fixed	&Fixed::operator=(const Fixed &f) {
+    std::cout << "Assignement operator called" << std::endl;
+    if (this != &f) {
+        this->_value = f.getRawBits();
+    }
+    return *this;
+}
+
+int	Fixed::getRawBits(void)const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_value;
+}
+
+void	Fixed::setRawBits(const int raw) {
+    Fixed::_value = raw;
+    return;
+}
---
+++ Module 02/ex01/Fixed.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 13:10:16 by dclark            #+#    #+#             */
+/*   Updated: 2022/02/23 15:14:04 by dclark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+# define FIXED_H
+
+#include <iostream>
+
+class Fixed {
+	public:
+		Fixed(void);
+		Fixed(const int value);
+		Fixed(const float value);
+		Fixed(const Fixed &f);
+		~Fixed(void);
+		Fixed &operator=(const Fixed &f);
+		int		getRawBits(void)const;
+		void	setRawBits(const int raw);
+		int		toInt(void)const;
+		float	toFloat(void)const;
+	private:
+		int					_value;
+		static const int	_binary = 8;	
+};
+
+#endif
