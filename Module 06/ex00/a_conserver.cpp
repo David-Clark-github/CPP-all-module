1d75316df98cc9d5b4b097d441cfa682bfd1040d
dclark <dclark@student.42.fr>
2022-04-13 17:47:56 +0200

    go save

---
+++ Module 06/ex00/a_conserver.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <stdio.h>
+
+int main(void) {
+	printf("sizeof: %lu\n", sizeof(char *));
+	printf("sizeof: %lu\n", sizeof(int));
+	printf("sizeof: %lu\n", sizeof(float));
+	printf("sizeof: %lu\n", sizeof(double));
+}
