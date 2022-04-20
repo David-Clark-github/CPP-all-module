5d33b2ddb23d1b5935f2e8ccc809a28c0f0c9949
dclark <dclark@student.42.fr>
2022-04-20 13:38:53 +0200

    Module 07 fait sans test

--- Module 07/ex02/includes/Array.h
+++ Module 07/ex02/includes/Array.h
@@ -6,7 +6,7 @@
 /*   By: david <dclark@student.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 14:56:01 by david             #+#    #+#             */
-/*   Updated: 2022/04/19 20:28:48 by david            ###   ########.fr       */
+/*   Updated: 2022/04/20 13:38:14 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,66 @@ class Array {
 
 		void	setSize(const size_t size);
 		size_t	getSize(void)const;
-		T	operator[](const size_t index);
+		T &	operator[](const size_t index);
 	private:
 		size_t	_size;
 		T	*	_element;
 };
 
+template<typename T>
+Array<T>::Array() {
+	return;
+}
 
+template<typename T>
+Array<T>::Array(unsigned int n) {
+	this->_element = new T[n];
+	return;
+}
+
+template<typename T>
+Array<T>::Array(Array const & a) {
+	*this = a;
+	return;
+}
+
+template<typename T>
+Array<T>& Array<T>::operator=(Array const & a) {
+	if (this != &a) {
+		for (size_t i = 0; i < a.getSize(); i++)
+			this->_element[i] = a._element[i];
+		this->setSize(a.getSize());
+	}
+	return *this;
+}
+
+template<typename T>
+Array<T>::~Array(void) {
+	delete [] this->_element;
+	return;
+}
+
+template<typename T>
+void	Array<T>::setSize(const size_t size) {
+	this->_size = size;
+	return;
+}
+
+template<typename T>
+size_t	Array<T>::getSize(void)const {
+	return this->_size;
+}
+
+
+template<typename T>
+T &	Array<T>::operator[](const size_t index) {
+	try {
+		if (index > getSize() - 1) {
+			throw std::exception();
+		}
+	} catch (std::exception& e){
+		std::cout << e.what() << std::endl;
+		std::cout << "l'index est invalid" << std::endl;
+	}
+	return this->_element[index];
+}
--- Module 07/ex02/src/main.cpp
+++ Module 07/ex02/src/main.cpp
@@ -6,15 +6,25 @@
 /*   By: david <dclark@student.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 14:51:17 by david             #+#    #+#             */
-/*   Updated: 2022/04/19 23:10:36 by david            ###   ########.fr       */
+/*   Updated: 2022/04/20 13:06:46 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Array.h>
 
 int main(void) {
-	Array	<int>t(5);
-	int	res = t[1];
-	std::cout << res << std::endl;
+	unsigned int	size = 8;
+	Array	<int>t(size);
+	Array	<int>u(size);
+	for (unsigned int i = 0; i < size; i++) {
+		t[i] = (i * 2);
+	}
+	u = t;
+	for (unsigned int i = 0; i < size; i++) {
+		std::cout << t[i] << std::endl;
+	}
+	for (unsigned int i = 0; i < size; i++) {
+		std::cout << u[i] << std::endl;
+	}
 	return 0;
- }
+}
